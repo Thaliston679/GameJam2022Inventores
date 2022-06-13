f9654219b85d2b30bbed705007230d4a852d226b
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-13 13:57:55 -0300

    Script de dano no inimigo

--- Assets/Scripts/EnemyLifeAndDeath.cs
+++ Assets/Scripts/EnemyLifeAndDeath.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class EnemyLifeAndDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public GameObject smokeExplosion;
+    public int enemyHP;
+    private float selfTimeDamage = 0;
+    private bool onDamage = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (enemyHP <= 0)
+        {
+            //GameObject explosion = Instantiate(smokeExplosion, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+        TimerDamage();
+    }
+
+    void TimerDamage()
+    {
+        if (selfTimeDamage < 1)
+        {
+            selfTimeDamage += Time.deltaTime;
+        }
+        if (onDamage)
+        {
+            if (selfTimeDamage > 0.2f)
+            {
+                SpriteRenderer img = gameObject.GetComponent<SpriteRenderer>();
+                img.color = Color.white;
+
+                selfTimeDamage = 0;
+
+                onDamage = false;
+            }
+        }
+
+    }
+
+    public void SetEnemyHp(int i)
+    {
+        enemyHP = i;
+    }
+
+    public int GetEnemyHp()
+    {
+        return enemyHP;
+    }
+
+    public void SetSelfTimerDamage(float i)
+    {
+        selfTimeDamage = i;
+    }
+
+    public void SetOnDamage(bool i)
+    {
+        onDamage = i;
     }
 }
