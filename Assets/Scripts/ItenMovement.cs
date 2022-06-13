b5a14b1f23e21c41c9c724e27bf2d2bb217df52e
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-13 16:48:27 -0300

    Itens com movimentação adicionados

---
+++ Assets/Scripts/ItenMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItenMovement : MonoBehaviour
+{
+    public bool moveUp = true;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    IEnumerator Revert()
+    {
+        yield return new WaitForSeconds(1f);
+        moveUp = false;
+        yield return new WaitForSeconds(1f);
+        moveUp = true;
+    }
+    void Update()
+    {
+        if (moveUp)
+        {
+            StartCoroutine("Revert");
+            if (transform.position.y > startPosition.y + 1f)
+            {
+                transform.position = new Vector2(transform.position.x, startPosition.y + 0.3f);
+            }
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.3f * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.3f * Time.deltaTime);
+        }
+    }
+
+    public Vector3 GetStartPosistion()
+    {
+        return startPosition;
+    }
+}
