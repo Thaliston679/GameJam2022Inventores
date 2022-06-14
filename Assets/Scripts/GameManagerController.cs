04a417e50e56765866b64de973efb59a940e2390
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2022-06-14 10:41:49 -0300

    HUD e GameManagerController

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+    public static void PauseGame()
+    {
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
